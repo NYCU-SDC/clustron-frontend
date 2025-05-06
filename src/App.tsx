934237a99bacc5eff6dc805cbf494b7143a51b8c
Yuhan Qiu <h237k77777@gmail.com>
2025-05-06 18:39:57 +0800

    feat: add basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Onboarding from "@/pages/Onboarding";
+import Setting from "./pages/Setting";
 import { Toaster } from "@/components/ui/sonner";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Onboarding" element={<Onboarding />} />
+        <Route path="/Setting" element={<Setting />} />
       </Routes>
       <Toaster />
     </>
