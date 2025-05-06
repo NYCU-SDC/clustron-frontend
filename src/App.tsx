c33337607ac36b8806dc39975a862227fb4f9fb1
Yuhan Qiu <h237k77777@gmail.com>
2025-05-06 19:10:42 +0800

    fix: remove Dashboard used for dev in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Callback from "@/pages/Callback";
-import Dashboard from "@/pages/Dashboard";
 
 export default function App() {
   return (
@@ -10,7 +9,6 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/callback" element={<Callback />} />
-      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
