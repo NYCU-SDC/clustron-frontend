4abd0c3fdf8212af436718c1c648a62ac53f9f89
Yuhan Qiu <h237k77777@gmail.com>
2025-05-10 21:06:28 +0800

    chore: improve navbar and sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,21 @@
 import { NavLink } from "react-router";
-import ModeToggle from "./ModeToggle";
-import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  // DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  // DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import ModeToggle from "@/components/ModeToggle";
+import ProfileImg from "@/assets/Profile.png";
 
 export default function Navbar() {
   return (
@@ -14,12 +29,12 @@ export default function Navbar() {
             to="/Groups"
             className={({ isActive }) =>
               [
-                "text-base",
+                "text-base hover:bg-gray-100",
                 "px-3 py-2 rounded-lg",
-                isActive
-                  ? "text-gray-500"
-                  : "hover:bg-gray-100 dark:hover:bg-gray-600",
-              ].join(" ")
+                !isActive && "text-gray-500",
+              ]
+                .filter(Boolean)
+                .join(" ")
             }
           >
             Groups
@@ -28,12 +43,12 @@ export default function Navbar() {
             to="/Settings"
             className={({ isActive }) =>
               [
-                "text-base",
+                "text-base hover:bg-gray-100",
                 "px-3 py-2 rounded-lg",
-                isActive
-                  ? "text-gray-500"
-                  : "hover:bg-gray-100 dark:hover:bg-gray-700",
-              ].join(" ")
+                !isActive && "text-gray-500",
+              ]
+                .filter(Boolean)
+                .join(" ")
             }
           >
             Settings
@@ -41,12 +56,30 @@ export default function Navbar() {
         </div>
         <div className="flex items-center space-x-4">
           <ModeToggle />
-          <Button
-            variant="ghost"
-            className="text-base px-3 py-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700"
-          >
-            Logout
-          </Button>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage src={ProfileImg} alt="User" />
+                <AvatarFallback>S</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuGroup>
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger>Language</DropdownMenuSubTrigger>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent>
+                      <DropdownMenuItem>English</DropdownMenuItem>
+                      <DropdownMenuItem>繁體中文</DropdownMenuItem>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Log out</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </nav>
--- src/components/SettingSidebar.tsx
+++ src/components/SettingSidebar.tsx
@@ -3,16 +3,19 @@ import { NavLink } from "react-router";
 export default function SettingSideBar() {
   return (
     <aside className="mx-15 my-8">
-      <div className="text-3xl font-semibold mb-8">Settings</div>
+      <div className="text-4xl font-semibold mb-8">Settings</div>
       <ul className="space-y-4">
         <li>
           <NavLink
             to={`/Settings/General`}
             className={({ isActive }) =>
               [
-                "pl-1 py-2 rounded-lg text-sm",
-                isActive ? "text-gray-500" : "text-black",
-              ].join(" ")
+                "text-base hover:bg-gray-100",
+                "pl-1 py-2 rounded-lg",
+                !isActive && "text-gray-500",
+              ]
+                .filter(Boolean)
+                .join(" ")
             }
           >
             General
@@ -23,9 +26,12 @@ export default function SettingSideBar() {
             to={`/Settings/SSH`}
             className={({ isActive }) =>
               [
-                "pl-1 py-2 rounded-lg text-sm",
-                isActive ? "text-gray-500" : "text-black",
-              ].join(" ")
+                "text-base hover:bg-gray-100",
+                "pl-1 py-2 rounded-lg",
+                !isActive && "text-gray-500",
+              ]
+                .filter(Boolean)
+                .join(" ")
             }
           >
             SSH
