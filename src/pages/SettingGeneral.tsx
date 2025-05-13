af0652ee8bf2af5a4bc6e3e999274693abe54ee0
Yuhan Qiu <h237k77777@gmail.com>
2025-05-13 20:47:40 +0800

    chore: resize card

--- src/pages/SettingGeneral.tsx
+++ src/pages/SettingGeneral.tsx
@@ -2,7 +2,7 @@ import SettingNameForm from "@/components/SettingNameForm";
 
 export default function SettingGeneral() {
   return (
-    <div className="w-full max-w-5xl px-6 py-8">
+    <div className="w-full max-w-3xl px-6 py-8">
       <SettingNameForm />
     </div>
   );
--- src/pages/SettingSSH.tsx
+++ src/pages/SettingSSH.tsx
@@ -3,7 +3,7 @@ import SettingKeyTable from "@/components/SettingKeyTable";
 
 export default function SettingSsh() {
   return (
-    <div className="w-full max-w-5xl px-6 py-8 flex flex-col gap-10">
+    <div className="w-full max-w-3xl px-6 py-8 flex flex-col gap-10">
       <SettingUsernameForm />
       <SettingKeyTable />
     </div>
