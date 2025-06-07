d2fe9a89380384f034184594f2e2a3a18dc947c7
Yuhan Qiu <h237k77777@gmail.com>
2025-06-07 18:32:37 +0800

    chore: revise css style for onboarding form min-width

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -2,7 +2,7 @@ import OnboardingForm from "@/components/OnboardingForm";
 
 export default function Onboarding() {
   return (
-    <div className="mx-auto my-50 w-full min-w-[23rem] max-w-md">
+    <div className="mx-auto my-50 w-full min-w-xs max-w-md">
       <OnboardingForm />
     </div>
   );
