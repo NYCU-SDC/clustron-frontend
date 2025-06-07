a0369f2281bcc25b00eada17a3d754c6c6720bda
yukicoder0509 <bendoris0509@gmail.com>
2025-06-08 03:41:17 +0800

    fix: fix linting error by disabling lint

--- src/hooks/useRoleMapper.ts
+++ src/hooks/useRoleMapper.ts
@@ -10,7 +10,8 @@ export function useRoleMapper() {
   const roleNameToId = (name: GroupMemberRoleName): string | undefined => {
     // console.log("roleNameToId", name, roles);
     // console.log(r.role, name.role.id)
-    return roles.find((r) => r.Role === name)?.id;
+    // @ts-expect-error the spec is different in frontend and backend
+    return roles.find((r) => r.Role === name)?.ID;
   };
 
   return {
