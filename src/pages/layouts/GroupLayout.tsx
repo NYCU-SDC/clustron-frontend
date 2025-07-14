40c4c2c1108a4a4b38eb4ec509f6844ba21b6cff
hoho56422 <annie10731303@gmail.com>
2025-07-15 02:17:54 +0800

    fix: scroll nav problem & group sidebar fix

--- src/pages/layouts/GroupLayout.tsx
+++ src/pages/layouts/GroupLayout.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
+import GroupSideBar from "@/components/group/GroupSideBar";
 
 export default function GroupLayout() {
   return (
-    <>
-      <main className=" flex w-full justify-center ">
+    <div className="flex w-full min-h-screen">
+      <aside className="sticky top-6  border-r z-10">
+        <GroupSideBar title="Group" />
+      </aside>
+
+      <main className=" flex w-full justify-center mt-20">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
