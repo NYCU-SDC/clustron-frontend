ca46cc622a816543d1d47b9343bead27ed405f1e
yukicoder0509 <bendoris0509@gmail.com>
2025-07-15 23:29:53 +0800

    fix: correct roleName property name in group member table

--- src/components/group/GroupMemberTable.tsx
+++ src/components/group/GroupMemberTable.tsx
@@ -129,7 +129,7 @@ export default function GroupMemberTable({
                     name={m.fullName}
                     id={m.studentId}
                     email={m.email}
-                    roleName={m.role.Role as GroupMemberRoleName}
+                    roleName={m.role.roleName as GroupMemberRoleName}
                     accessLevel={accessLevel}
                     showActions={canEditMembers && !isOverview}
                     isArchived={isArchived}
