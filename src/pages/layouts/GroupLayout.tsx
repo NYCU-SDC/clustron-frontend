24a50dcc547bf1c12d6fe327bf1128837a7600ec
hoho56422 <annie10731303@gmail.com>
2025-07-25 15:18:03 +0800

    fix: revert main brnach file

--- src/pages/layouts/GroupLayout.tsx
+++ src/pages/layouts/GroupLayout.tsx
@@ -1,16 +1,11 @@
 import { Outlet } from "react-router-dom";
-import GroupSideBar from "@/components/group/GroupSideBar";
 
 export default function GroupLayout() {
   return (
-    <div className="flex w-full min-h-screen">
-      <aside className="sticky top-6  border-r z-10">
-        <GroupSideBar title="Group" />
-      </aside>
-
-      <main className=" flex w-full justify-center mt-20">
+    <>
+      <main className=" flex w-full justify-center">
         <Outlet />
       </main>
-    </div>
+    </>
   );
 }
