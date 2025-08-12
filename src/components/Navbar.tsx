b9869f85014be85df0575f01c29c1494ea42e70c
Yuhan Qiu <h237k77777@gmail.com>
2025-08-12 19:32:13 +0800

    feat: initially completed the ui of BindLoginForm

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -78,7 +78,7 @@ export default function Navbar() {
           <ColorModeToggle />
           {isLoggedIn() ? (
             <DropdownMenu>
-              <DropdownMenuTrigger>
+              <DropdownMenuTrigger className="cursor-pointer">
                 <CircleUserRound />
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end">
