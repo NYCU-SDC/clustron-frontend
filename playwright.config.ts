73d756927e6eb660ee27de3790f35b3a53967288
yukicoder0509 <bendoris0509@gmail.com>
2025-09-11 13:43:36 +0800

    test: auto start the frontend server when the test start

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "vite --mode development",
     url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
