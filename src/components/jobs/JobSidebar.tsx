894df45f0a1b3b7c5170b09b812c829b0869350a
nyan <nyan222727@gmail.com>
2025-09-22 21:26:46 +0800

    JobSidebar are refractored

--- src/components/jobs/JobSidebar.tsx
+++ src/components/jobs/JobSidebar.tsx
@@ -1,41 +1,16 @@
-import { NavLink } from "react-router";
-import { useTranslation } from "react-i18next";
+import SideBar, { NavItem } from "@/components/ui/sidebar.tsx";
 
-function navLinkClass(isActive: boolean) {
-  return [
-    "text-base",
-    "pl-1 py-2 rounded-lg",
-    !isActive && "text-gray-500 dark:text-gray-400",
-  ]
-    .filter(Boolean)
-    .join(" ");
-}
+export default function GroupSideBarContainer({ title }: { title: string }) {
+  const jobNavItems: NavItem[] = [
+    {
+      to: `/joblist`,
+      labelKey: "jobsSideBar.ListNavLink",
+    },
+    {
+      to: `/jobform"`,
+      labelKey: "jobsSideBar.SubmitNavLink",
+    },
+  ];
 
-export default function JobSidebar() {
-  const { t } = useTranslation();
-  return (
-    <aside className="sticky top-[7rem] self-start min-w-48 ml-15 my-8">
-      <div className="text-4xl font-semibold mb-8">
-        {t("jobsSideBar.title")}
-      </div>
-      <ul className="space-y-4">
-        <li>
-          <NavLink
-            to="/joblist"
-            className={({ isActive }) => navLinkClass(isActive)}
-          >
-            {t("jobsSideBar.ListNavLink")}
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/jobform"
-            className={({ isActive }) => navLinkClass(isActive)}
-          >
-            {t("jobsSideBar.SubmitNavLink")}
-          </NavLink>
-        </li>
-      </ul>
-    </aside>
-  );
+  return <SideBar title={title} navItems={jobNavItems} className="min-w-36" />;
 }
