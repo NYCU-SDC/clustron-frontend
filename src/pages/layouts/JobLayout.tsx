454bf27d7e2473d6fca10c652039539d17ccf8e6
nyan <nyan222727@gmail.com>
2025-09-28 13:15:50 +0800

    fix: upload error

--- src/pages/layouts/JobLayout.tsx
+++ src/pages/layouts/JobLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import JobSidebar from "@/components/jobs/JobSidebar";
+import { useTranslation } from "react-i18next";
 
 export default function JobLayout() {
+  const { t } = useTranslation();
+
   return (
     <div className="flex w-full">
       <div className="min-w-xs border-r">
-        <JobSidebar />
+        <JobSidebar title={t("settingSideBar.title")} />
       </div>
       <main className="flex-1 flex justify-center">
         <Outlet />
