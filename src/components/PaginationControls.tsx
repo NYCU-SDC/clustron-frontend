a9fca27cb7c2bb6765faac64c82b8c2db5f6f3b9
nyan222727 <nyan222727@gmail.com>
2025-10-12 13:33:42 +0800

    fix:remove unused comments and code

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -12,7 +12,6 @@ import { usePaginationRange } from "@/hooks/usePaginationRange";
 type Props = {
   currentPage: number;
   totalPages: number;
-  // 设置状态的函数类型
   setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
 };
 
@@ -21,11 +20,9 @@ const PaginationControls: React.FC<Props> = ({
   totalPages,
   setCurrentPage,
 }) => {
-  // 使用 Hook 获取分页范围
   const { range, showStartEllipsis, showEndEllipsis } = usePaginationRange(
     currentPage,
     totalPages,
-    4, // 保持 maxVisiblePages 为 4
   );
 
   if (totalPages <= 1) {
@@ -36,7 +33,6 @@ const PaginationControls: React.FC<Props> = ({
     <div className="mt-6 flex justify-center">
       <Pagination>
         <PaginationContent>
-          {/* 上一页按钮 */}
           <PaginationItem>
             <PaginationPrevious
               onClick={() => setCurrentPage((p) => Math.max(p - 1, 0))}
@@ -46,33 +42,29 @@ const PaginationControls: React.FC<Props> = ({
             />
           </PaginationItem>
 
-          {/* 起始省略号 */}
           {showStartEllipsis && (
             <PaginationItem>
               <span className="px-2">...</span>
             </PaginationItem>
           )}
 
-          {/* 页码链接 */}
           {range.map((page) => (
             <PaginationItem key={page}>
               <PaginationLink
                 isActive={page === currentPage}
                 onClick={() => setCurrentPage(page)}
               >
-                {page + 1} {/* 0-based 索引转为 1-based 页码 */}
+                {page + 1}
               </PaginationLink>
             </PaginationItem>
           ))}
 
-          {/* 结束省略号 */}
           {showEndEllipsis && (
             <PaginationItem>
               <span className="px-2">...</span>
             </PaginationItem>
           )}
 
-          {/* 下一页按钮 */}
           <PaginationItem>
             <PaginationNext
               onClick={() =>
