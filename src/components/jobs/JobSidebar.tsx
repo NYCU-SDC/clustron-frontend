ea907fd33a75bc03da72be8cc4676866ed2021d1
nyan222727 <nyan222727@gmail.com>
2025-11-02 21:47:18 +0800

    manually add JobSidebar component

---
+++ src/components/jobs/JobSidebar.tsx
@@ -0,0 +1,41 @@
+import { NavLink } from "react-router";
+import { useTranslation } from "react-i18next";
+
+function navLinkClass(isActive: boolean) {
+  return [
+    "text-base",
+    "pl-1 py-2 rounded-lg",
+    !isActive && "text-gray-500 dark:text-gray-400",
+  ]
+    .filter(Boolean)
+    .join(" ");
+}
+
+export default function JobSidebar() {
+  const { t } = useTranslation();
+  return (
+    <aside className="sticky top-[7rem] self-start min-w-48 ml-15 my-8">
+      <div className="text-4xl font-semibold mb-8">
+        {t("jobsSideBar.title")}
+      </div>
+      <ul className="space-y-4">
+        <li>
+          <NavLink
+            to="/jobs"
+            className={({ isActive }) => navLinkClass(isActive)}
+          >
+            {t("jobsSideBar.ListNavLink")}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/jobs/submit"
+            className={({ isActive }) => navLinkClass(isActive)}
+          >
+            {t("jobsSideBar.SubmitNavLink")}
+          </NavLink>
+        </li>
+      </ul>
+    </aside>
+  );
+}
