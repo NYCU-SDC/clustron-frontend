4e9d19a0019eefffd35b7bbceb8eb42a72582143
nyan222727 <nyan222727@gmail.com>
2025-11-02 19:50:00 +0800

    refactor pagination in GroupMemberTable.tsx

--- src/components/group/PendingMemberTable.tsx
+++ src/components/group/PendingMemberTable.tsx
@@ -7,14 +7,6 @@ import {
   TableHead,
   TableBody,
 } from "@/components/ui/table";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
 import PendingMemberRow from "@/components/group/PendingMemberRow";
 import { getGroupPermissions } from "@/lib/groupPermissions";
 import { useJwtPayload } from "@/hooks/useJwtPayload";
@@ -26,6 +18,7 @@ import type { GroupRoleAccessLevel, GroupMemberRoleName } from "@/types/group";
 import { GlobalRole } from "@/lib/permission";
 import { AccessLevelUser } from "@/types/group";
 import { Loader2 } from "lucide-react";
+import PaginationControls from "@/components/PaginationControls";
 type Props = {
   groupId: string;
   accessLevel?: GroupRoleAccessLevel;
@@ -137,59 +130,11 @@ export default function PendingMemberTable({
             </Table>
 
             <div className="mt-6 flex justify-center">
-              <Pagination>
-                <PaginationContent>
-                  <PaginationItem>
-                    <PaginationPrevious
-                      onClick={() => setCurrentPage((p) => Math.max(p - 1, 0))}
-                      className={
-                        currentPage === 0
-                          ? "opacity-50 pointer-events-none"
-                          : ""
-                      }
-                    />
-                  </PaginationItem>
-
-                  {startPage > 0 && (
-                    <PaginationItem>
-                      <span className="px-2">...</span>
-                    </PaginationItem>
-                  )}
-
-                  {Array.from(
-                    { length: endPage - startPage + 1 },
-                    (_, i) => startPage + i,
-                  ).map((page) => (
-                    <PaginationItem key={page}>
-                      <PaginationLink
-                        isActive={page === currentPage}
-                        onClick={() => setCurrentPage(page)}
-                      >
-                        {page + 1}
-                      </PaginationLink>
-                    </PaginationItem>
-                  ))}
-
-                  {endPage < totalPages - 1 && (
-                    <PaginationItem>
-                      <span className="px-2">...</span>
-                    </PaginationItem>
-                  )}
-
-                  <PaginationItem>
-                    <PaginationNext
-                      onClick={() =>
-                        setCurrentPage((p) => Math.min(p + 1, totalPages - 1))
-                      }
-                      className={
-                        currentPage === totalPages - 1
-                          ? "opacity-50 pointer-events-none"
-                          : ""
-                      }
-                    />
-                  </PaginationItem>
-                </PaginationContent>
-              </Pagination>
+              <PaginationControls
+                currentPage={currentPage}
+                totalPages={totalPages}
+                setCurrentPage={setCurrentPage}
+              />
             </div>
           </>
         )}
