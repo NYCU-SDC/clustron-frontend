b7dcc9f22f6af078d6c9f458fdd4a350d8fb7f39
LI, GE-ER <yahappygrace@gmail.com>
2026-03-08 15:50:35 +0800

    fix: hide Group Settings for users and adjust card into middle

--- src/pages/layouts/GroupLayout.tsx
+++ src/pages/layouts/GroupLayout.tsx
@@ -3,10 +3,17 @@ import { useGetGroupById } from "@/hooks/useGetGroupById";
 import SideBar, { NavItem } from "@/components/Sidebar";
 import { useTranslation } from "react-i18next";
 import ErrorBoundary from "@/components/ErrorBoundary";
+import { GlobalRole } from "@/lib/permission";
+import { getGroupPermissions } from "@/lib/groupPermissions";
+import { useJwtPayload } from "@/hooks/useJwtPayload";
 
 export default function GroupLayout() {
   const { id } = useParams<{ id: string }>();
   const { data: group, isLoading } = useGetGroupById(id!);
+  const payload = useJwtPayload();
+  const accessLevel = group?.me.role.accessLevel;
+  const globalRole = payload?.Role as GlobalRole;
+  const isReadonly = getGroupPermissions(accessLevel, globalRole).isReadonly;
   const { t } = useTranslation();
 
   const groupNavItems: NavItem[] = [
@@ -27,7 +34,11 @@ export default function GroupLayout() {
       <div className="min-w-xs border-r px-4">
         <SideBar
           title={group.title}
-          navItems={groupNavItems}
+          navItems={
+            isReadonly
+              ? groupNavItems.filter((_, index) => index !== 1)
+              : groupNavItems
+          }
           className="min-w-36"
         />
       </div>
