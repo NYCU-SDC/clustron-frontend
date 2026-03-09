9239c02bf9c74acddaad75ea362a59b805394f5d
yukicoder0509 <bendoris0509@gmail.com>
2026-03-09 13:42:30 +0800

    feat: implement mobile version in setting/host connection information page

--- src/pages/setting/SettingSSH.tsx
+++ src/pages/setting/SettingSSH.tsx
@@ -4,7 +4,7 @@ import SettingKeyTable from "@/components/setting/SettingKeyTable";
 
 export default function SettingSSH() {
   return (
-    <div className="w-full min-w-lg max-w-2xl px-6 py-8 flex flex-col gap-10">
+    <div className="w-full max-w-2xl px-4 md:px-6 py-6 md:py-8 flex flex-col gap-6 md:gap-10">
       <SettingLinuxUsernameForm />
       <UpdatePasswordForm />
       <SettingKeyTable />
